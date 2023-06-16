06d2cda89bdcd84b1ac44d36bdd6be81ccea590f
Inigo Hersanta Sutandyo <86969846+inigoSutandyo@users.noreply.github.com>
2023-06-16 07:24:15 +0700

    add logic to prevent error during split container

--- src/main/BSP.java
+++ src/main/BSP.java
@@ -17,6 +17,8 @@ public class BSP {
 		Node newNode = new Node(container, iter, parent);
 		if (iter <= iteration) {
 			Container[] res = splitContainer(container);
+			if (res[0] == null || res[1] == null) return newNode;
+			
 			newNode.setLeft(split(res[0], iter+1, newNode));
 			newNode.setRight(split(res[1], iter+1, newNode));
 		}
@@ -24,9 +26,14 @@ public class BSP {
 	}
 	
 	public Container[] splitContainer(Container c) {
-		Container c1, c2;
-		int r = random(1, 3);
-//		System.out.println(r);
+		Container c1 = null, c2 = null;
+		Container[] res = {c1, c2};
+		int r = splitDirection(random(1, 3), c.getWidth(), c.getHeight());
+		
+		if (r == -1) {
+			System.out.println("Cannot be split");
+			return res;
+		} 
 		if (r == 1) {
 			// split vertical
 			int w1 = random(1, c.getWidth());
@@ -38,12 +45,25 @@ public class BSP {
 			c1 = new Container(c.getX(), c.getY(), c.getWidth(), h1); 
 			c2 = new Container(c.getX(), c.getY() + h1, c.getWidth(), c.getHeight() - h1); 
 		}
-		Container[] res = {c1, c2};
+		res[0] = c1;
+		res[1] = c2;
+		
 		return res;
 	}
 	
 
-
+	private int splitDirection(int r, int w, int h) {
+		if (w <= 1 && h <= 1) {
+			return -1;
+		} else if (w <= 1) {
+			return 2;
+		} else if (h <= 1) {
+			return 1;
+		} else {
+			return r;
+		}
+	}
+	
 	int random(int min, int max) {
 		if (max == min) {
 			return max;
